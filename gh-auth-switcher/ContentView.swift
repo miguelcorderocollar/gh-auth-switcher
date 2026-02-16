15d77cbad08b5b0749a77f57553dbe7a0cdceaad
Miguel Cordero Collar <miguelcorderocollar@redbull.com>
2026-02-16 08:45:29 +0100

    Initial Commit

---
+++ gh-auth-switcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gh-auth-switcher
+//
+//  Created by Miguel Cordero Collar on 16.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gh-auth-switcher/gh_auth_switcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  gh_auth_switcherApp.swift
+//  gh-auth-switcher
+//
+//  Created by Miguel Cordero Collar on 16.02.26.
+//
+
+import SwiftUI
+
+@main
+struct gh_auth_switcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
